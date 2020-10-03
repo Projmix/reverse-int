4c0560d8c62ca5d388cf125c491c823c902c1013
ProjmiX <balb310372@mail.ru>
2020-10-03 21:59:41 +0300

    feat: add solution for task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function reverse (n) {
-  
+    if (n < 0 ) {
+    n = Math.abs(n);
+    }
+    let num, k=0, rez = "";
+    let i = n;
+    while(i>0){
+        i = Math.floor(i/10);
+        k++;
+    }
+    for(k;k>0;k--){
+    num = n % 10;
+    n = Math.floor(n/10);
+    rez = rez + num;
+    }
+    return rez;
 }
